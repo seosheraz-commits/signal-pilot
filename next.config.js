080487a7190b49a0de27a4ac00194f66f00970ee
seosheraz-commits <seosheraz@gmail.com>
2025-09-07 17:19:34 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: { ignoreDuringBuilds: true }
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true }
 };
 module.exports = nextConfig;
